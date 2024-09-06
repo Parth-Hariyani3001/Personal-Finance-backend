d0f67ceb9e0c7ebe3e12a4800ffafc3f9e8c469b
Parth-Hariyani3001 <hariyaniparth-inft@atharvacoe.ac.in>
2024-09-06 23:18:54 +0530

    Implemented dotenv and nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.get("/",(req,res) => {
+    console.log(process.env.NAME);
+    console.log("Hello")
     res.send("Hello Typescript Server");
 })
 
