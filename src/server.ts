02e5ebd63dc25a735e5e69ca0044ecd182368ce6
Parth-Hariyani3001 <hariyaniparth-inft@atharvacoe.ac.in>
2024-09-08 00:38:44 +0530

    Request and Response Types

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+
+import express,{ Request, Response }  from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,7 +7,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.get("/",(req,res) => {
+app.get("/",(req: Request ,res: Response) => {
     console.log(process.env.NAME);
     console.log("Hello")
     res.send("Hello Typescript Server");
